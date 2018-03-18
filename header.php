d67423f08046d28a3ae6a9c5a7d9db21e4e2af24
egwelch <egwelch@users.noreply.github.com>
2018-03-18 18:21:55 -0400

    classwork3
    Updates

--- header.php
+++ header.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html >
 <html lang="en">
-
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,8 +8,8 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="./css/webd2201.css" /> 
 
-  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" />
-  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" />
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
   	<!--
 	Author: Elizabeth Welch
@@ -22,7 +21,6 @@
 	-->
 	<title><?php echo "$title"; ?></title>
 </head>
-
 <body>
 	<div class="container">
 	<nav class="navbar navbar-inverse">
